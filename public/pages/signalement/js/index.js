d3d8fa8a82e2e413b3bee9a3606dc85ca91efc29
Allan BUSI <busi.travail@gmail.com>
2020-06-24 13:56:18 +0200

    fini

---
+++ public/pages/signalement/js/index.js
@@ -0,0 +1,28 @@
+$.ajax(settings).done(function(response) {
+    console.log(response);
+});
+
+const settings = {
+    "url ": "https://obscure-sands-01552.herokuapp.com/report" + localStorage.getItem("token"),
+    "method": "GET",
+    "headers": {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+};
+
+$.ajax(settings).done((response) => { // <>200
+    if (response === false) {
+        localStorage.clear()
+        document.location.href = "/";
+    } else
+        $('#user').text(JSON.stringify(response))
+}).catch((err) => { // <> 400
+    localStorage.clear()
+    document.location.href = "/";
+})
+
+$('#deco').click(() => {
+
+    localStorage.clear()
+    document.location.href = "/";
+})
